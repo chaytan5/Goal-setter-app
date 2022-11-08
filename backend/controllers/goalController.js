7e8d2b9a6fa31e4b15e39b81c03cd8ad433a3faf
Chaytan Arora <chaytanarora@gmail.com>
2022-11-09 02:57:38 +0530

    Added authentication and route protection using JWT

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const Goal = require("../models/goalModel");
 
@@ -6,7 +7,7 @@ const Goal = require("../models/goalModel");
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find();
+	const goals = await Goal.find({ user: req.user.id });
 
 	res.status(200).json(goals);
 });
@@ -22,6 +23,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
 	const goal = await Goal.create({
 		text: req.body.text,
+		user: req.user.id,
 	});
 
 	res.status(200).json(goal);
@@ -43,6 +45,19 @@ const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error("Please add a text field");
 	}
 
+	const user = await User.findById(req.user.id);
+
+	// Check the user
+	if (!user) {
+		res.status(401);
+		throw new Error("User not found");
+	}
+	// Check if the logged in user is same user
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error("User not authorized");
+	}
+
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	});
@@ -58,6 +73,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
 	if (!goal) {
 		res.status(400);
 		throw new Error("Goal not found");
+	}
+
+	const user = await User.findById(req.user.id);
+
+	// Check the user
+	if (!user) {
+		res.status(401);
+		throw new Error("User not found");
+	}
+	// Check if the logged in user is same as goal user
+	if (goal.user.toString() !== user.id) {
+		res.status(401);
+		throw new Error("User not authorized");
 	}
 
 	await goal.remove();
