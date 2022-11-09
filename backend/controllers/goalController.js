9c28f208c60e5be96587759e7e2a9d227efef898
Chaytan Arora <chaytanarora@gmail.com>
2022-11-09 21:25:43 +0530

    fix: backend minor bug fix

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -45,15 +45,13 @@ const updateGoal = asyncHandler(async (req, res) => {
 		throw new Error("Please add a text field");
 	}
 
-	const user = await User.findById(req.user.id);
-
 	// Check the user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error("User not found");
 	}
 	// Check if the logged in user is same user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		res.status(401);
 		throw new Error("User not authorized");
 	}
@@ -75,15 +73,13 @@ const deleteGoal = asyncHandler(async (req, res) => {
 		throw new Error("Goal not found");
 	}
 
-	const user = await User.findById(req.user.id);
-
 	// Check the user
-	if (!user) {
+	if (!req.user) {
 		res.status(401);
 		throw new Error("User not found");
 	}
 	// Check if the logged in user is same as goal user
-	if (goal.user.toString() !== user.id) {
+	if (goal.user.toString() !== req.user.id) {
 		res.status(401);
 		throw new Error("User not authorized");
 	}
